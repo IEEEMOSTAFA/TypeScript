b6214a46c0b7c357ac7fc35c96c838ea9924cb45
IEEEMOSTAFA <skmostafa8888@gmail.com>
2025-09-07 00:14:32 +0600

    dataType added

---
+++ built-in-type/index.js
@@ -0,0 +1,10 @@
+// built-in-type : number, string, boolean,void, undefined, null
+var userId;
+var userName;
+var isActivated;
+userId = 101;
+userName = "Mostafa";
+isActivated = true;
+console.log("UserId : ".concat(userId));
+console.log("UserName : ".concat(userName));
+console.log("Is Activated : ".concat(isActivated));
---
+++ built-in-type/index.ts
@@ -0,0 +1,14 @@
+// built-in-type : number, string, boolean,void, undefined, null
+
+let userId: number;
+let userName: string;
+let isActivated : boolean;
+
+userId = 101;
+userName = "Mostafa";
+isActivated = true;
+console.log(`UserId : ${userId}`);
+console.log(`UserName : ${userName}`);
+console.log(`Is Activated : ${isActivated}`);
+
+
