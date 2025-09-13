5100fd0850ff6f83ad33d53d5b2ac9b1465c50b2
IEEEMOSTAFA <skmostafa8888@gmail.com>
2025-09-13 13:14:04 +0600

    new file added

---
+++ user-defined-type/any-type.ts
@@ -0,0 +1,37 @@
+// let data: any;
+
+// data = 10;        // number
+// data = "hello";   // string
+// data = true;      // boolean
+// data = {x: 1};    // object
+
+
+// let data : any;
+// data = 11;
+// data = "mostafa";
+// data = true;
+// // data = [119,120];
+// // data = {x: 1, y: 2};
+// console.log(data);
+
+
+
+
+
+
+
+// TypeScript
+
+// let myArray: any[] = [];
+
+// myArray.push(200);
+// myArray.push(false);
+// myArray.push({ key: "GEEKSFORGEEKS" });
+
+// console.log(myArray);
+
+let myArray: any[] = [];
+myArray.push(200);
+myArray.push(true);
+myArray.push({key: "Mostafa"});
+console.log(myArray);
---
+++ user-defined-type/object-type.js
@@ -0,0 +1,15 @@
+// let user: object;
+// user = {name: "Mostafa", age: 24};
+// console.log(user);
+// for safety use :
+// let user1: {name: string, age: number};
+// user1 = {name:"Mostafa", age: 24};
+// console.log(user1);
+// const car: { type: string, model: string, year: number } = {
+//   type: "Toyota",
+//   model: "Corolla",
+//   year: 2009
+// };
+var car;
+car = { type: "Toyota", model: "Corolla", year: 2032 };
+console.log(car);
